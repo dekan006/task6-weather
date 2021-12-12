060a0a53c5ca332055106b040027035cac7bc9f7
dekan006 <dekan06@gmail.com>
2021-12-13 00:45:10 +0300

    clean comment

--- src/app/api-weather/api-weather.component.ts
+++ src/app/api-weather/api-weather.component.ts
@@ -14,32 +14,13 @@ export class ApiWeatherComponent implements OnInit {
   urlApiCityList = "http://api.airvisual.com/v2/cities?country=USA&key=2760c5ca-6442-4c6a-8874-b34fec353fce&state=California";
   urlApiCityWeather = "http://api.airvisual.com/v2/city?state=California&country=USA&key=2760c5ca-6442-4c6a-8874-b34fec353fce"
   
-  //condition:boolean=false;
-  // constructor() { 
-  //   this.cities = []
-  // }
-
+ 
   constructor(private http: HttpClient) {
     this.cities = [];
     this.cityWeather ='';
   }
 
-    // const domain = 'http://api.airvisual.com';
-    // const endPoint = 'v2/cities';
-    // const APIKey = '2760c5ca-6442-4c6a-8874-b34fec353fce';
-    // const APICIty = {
-    //   state: 'California',
-    //   country: 'USA',
-    //   key: APIKey,
-    // };
-
-    // const url = `${domain}${endPoint}`;
-
-
-    // this.http.get(url, { params: APICity }).pipe(map((response: any) => response.data)).subscribe((citys: any) => {
-    //   this.city = city.tp;
-    // });
-
+   
   showCity(){
     
     this.http.get<any>(this.urlApiCityList).subscribe(data => {  
@@ -48,16 +29,14 @@ export class ApiWeatherComponent implements OnInit {
   }
 
   getWeatherCity(city:string) {
-    //&city=Los%20Angeles
+    
     this.http.get(`${this.urlApiCityWeather}&city= ${city}`).subscribe((response:any) => {  
       this.cityWeather = `Temp: ${response.data.current.weather.tp} ℃ - Humidity: ${response.data.current.weather.hu} % - ${city}`;
       })
-      //this.condition=!this.condition;
+      
       console.log(city);
     
-    // https://airvisual.com/images/01d.png
-    // this.cityWeather = "21"
-    // console.log(city)
+    
   }
 
   ngOnInit(): void {
